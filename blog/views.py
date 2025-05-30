4f3a1de37d128132cf0d36e927060a9319f044bf
Sulayman Muhammad <sulayman.m.f@gmail.com>
2025-05-30 10:48:11 +0100

    the blog app has benn completed to a very reasnable extent

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 class BlogListView(ListView):
     template_name = 'home.html'
     model = Post
 class BlogDetailView(DetailView):
     model= Post
     template_name = 'post_details.html'
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+class BlogUpdateView(UpdateView):
+    template_name = 'post_edit.html'
+    model = Post
+    fields =['title', 'body']
+class BlogDeleteView(DeleteView):
+    template_name= 'post_delete.html'
+    model = Post
+    success_url = reverse_lazy('home')
